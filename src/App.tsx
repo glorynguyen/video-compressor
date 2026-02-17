94431f16866840a50a26435b8d6b71550ee8a381
Vinh Nguyen <nguyenbavinhcntt@gmail.com>
2026-02-17 15:19:15 +0700

    fix: revert app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,98 @@
-import { Compress } from 'compress-library';
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
+import coreURL from "@ffmpeg/core?url";
+import wasmURL from "@ffmpeg/core/wasm?url";
+import { Compress } from "@cheryx2020/core";
+import { Routes, Route, Link } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import LocalTool from "./pages/LocalTool";
+import "/node_modules/@cheryx2020/core/dist/index.css"
 
-// Type casting for Compress.CompressType
-const compressType: Compress.CompressType = Compress.CompressType;
+const sharedProps = {
+  FFmpeg,
+  fetchFile,
+  coreURL,
+  wasmURL,
+  showOptionsPanel: true,
+};
 
-// Update lines 87-89 to use the casted CompressType
-const conversionType1 = compressType.CONVERT;
-const conversionType2 = compressType.AUDIO;
-const conversionType3 = compressType.CUSTOM;
+function App() {
+  return <>
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand flex center" to="/">
+            <div className="logo"></div>
+            <div className="ml-10">Video Compressor</div>
+          </Link>
 
-// Rest of your code...
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                Compress Video
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/videotogif">
+                Convert to GIF
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/convert">
+                Convert Format
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/audio">
+                Extract Audio
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/custom">
+                Custom FFmpeg
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/localtool">
+                  Local Tool Guide
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Compress key={Compress.CompressType.COMPRESS} {...sharedProps} type={Compress.CompressType.COMPRESS} />} />
+        <Route path="/videotogif" element={<Compress key={Compress.CompressType.GIF} {...sharedProps} type={Compress.CompressType.GIF} />} />
+        <Route path="/convert" element={<Compress key={Compress.CompressType.CONVERT} {...sharedProps} type={Compress.CompressType.CONVERT} />} />
+        <Route path="/audio" element={<Compress key={Compress.CompressType.AUDIO} {...sharedProps} type={Compress.CompressType.AUDIO} />} />
+        <Route path="/custom" element={<Compress key={Compress.CompressType.CUSTOM} {...sharedProps} type={Compress.CompressType.CUSTOM} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/localtool" element={<LocalTool />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  </>
+}
+
+export default App
